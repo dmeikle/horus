91cd0dc3f491136e57f3e3b095e3ad9e0a19cf02
Dave Meikle <david@quantumunit.com>
2015-03-24 10:57:41 -0700

    added check for paramtype on listener execute

--- src/Gossamer/Horus/EventListeners/AbstractListener.php
+++ src/Gossamer/Horus/EventListeners/AbstractListener.php
@@ -15,15 +15,19 @@ class AbstractListener
     
     protected $listenerConfig = null;
     
-    public function __construct(Logger $logger, Request $request = null) {
+    public function __construct(Logger &$logger, Request &$request = null) {
         $this->logger = $logger;
         $this->request = $request;
     }
     
     public function execute($state, $params) {
         $method = 'on_' . $state;
-        $event = new Event($state, $params);
-        
+        $event = null;
+        if(!$params instanceof Event) {
+            $event = new Event($state, $params);
+        } else {
+            $event = $params;
+        }
        // $this->logger->addDebug('checking listener for method: ' . $method);
         if (method_exists($this, $method)) {
             call_user_func_array(array($this, $method), array($event));      
